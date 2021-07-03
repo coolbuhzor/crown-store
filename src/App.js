c155452affde60eab93b91730905ddb9ca0b989e
coolbuhzor <coolbuhzor@gmail.com>
2021-07-03 21:58:52 +0100

    started routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import HomePage from './pages/homepage/HomePage';
 
-
-
 function App() {
   return (
     <div className='App'>
--- src/components/menu-iem/MenuItem.js
+++ src/components/menu-iem/MenuItem.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 import './Menu-item.scss';
-
-
+import { withRouter } from 'react-router';
+// import Link from 'react-rou'
 
 const DirectDiv = styled.div`
- .background-image {
-     height: 100%;
-     width: 100%;
- background: url(${props => props.image})center/cover;
+  .background-image {
+    height: 100%;
+    width: 100%;
+    background: url(${(props) => props.image}) center/cover;
+  }
+`;
 
- }
-` 
-
-const MenuItem = ({title,imageUrl,size}) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
   return (
-    <DirectDiv image={imageUrl} className={`${size} menu-item`}>
-        <div className="background-image"/>
+    <DirectDiv
+      image={imageUrl}
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div className='background-image' />
       <div className='content'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitle'>Shop Now</span>
@@ -25,4 +28,4 @@ const MenuItem = ({title,imageUrl,size}) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
