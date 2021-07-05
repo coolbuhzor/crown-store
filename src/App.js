3a3392afa4bd00db4235a5bc950813b95f2061dd
coolbuhzor <coolbuhzor@gmail.com>
2021-07-06 00:45:04 +0100

    work continued

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import HomePage from './pages/homepage/HomePage';
 import './App.css'
 import {Switch, Route} from 'react-router-dom'
 import Shop from './pages/shop/shop';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className='App'>
+    <>
+    <Header/>
+    <Switch className='app'>
       <Route exact path='/' component={HomePage}/>
       <Route exact path='/shop' component={Shop}/>
-    </div>
+    </Switch>
+    </>
   );
 }
 
